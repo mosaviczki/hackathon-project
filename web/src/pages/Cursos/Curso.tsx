a4ea59dc554e9e6db5e3ee79ec10976aff030ea9
Gabriel Schorsch <gabrielschorschgsa@gmail.com>
2022-10-27 17:46:42 -0300

    added one more video

--- web/src/pages/Cursos/Curso.tsx
+++ web/src/pages/Cursos/Curso.tsx
@@ -44,6 +44,7 @@ const Curso = () => {
                 id: 2,
                 name: 'Variáveis',
                 description: 'Variáveis no curso de Programação',
+                video: <iframe width="560" height="315" src="https://www.youtube.com/embed/KxmRCc_Jris" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
             },
             {
                 id: 3,
